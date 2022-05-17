61b331d98f8fbd99d08a214f768a0e89f9e7246e
Zach Strout <zachstrout@gmail.com>
2022-05-17 15:16:32 -0500

    Implement gameboard module

---
+++ src/Modules/Templates/Gameboard/Gameboard.js
@@ -0,0 +1,63 @@
+import Ship from "../Ship/Ship.js"
+import _ from "lodash";
+
+class Gameboard {
+  constructor() {
+    this.BOARD_SIZE = 3;
+    this.board = Array.from(Array(this.BOARD_SIZE), () => new Array(this.BOARD_SIZE))
+    this.ships = [];
+    this.goodHits= [];
+    this.badHits = [];
+  }
+  placeShip(coords){
+    const diffX = Math.abs(coords.x1 - coords.x2);
+    const diffY = Math.abs(coords.y1 - coords.y2);
+    if (diffX > 0 && diffY > 0){
+      throw Error('It seems that the ship is diagonal?!?'); 
+    }
+    const shipLength = Math.max(diffX, diffY) + 1;
+    const shipIndex = this.ships.length;
+    this.ships.push(new Ship(shipLength));
+    const points = Gameboard.returnCoordsBetweenPoints(coords);
+    points.forEach((pointsPair, index)=>this.board[pointsPair.x][pointsPair.y]= {"ship":shipIndex, "shipGridIndex":index});
+  }
+
+  receiveAttack(coords){
+    if (this.goodHits.concat(this.badHits).find(hitsCoords=> _.isEqual(hitsCoords, coords)) === undefined){
+      const gridObject = this.board[coords.x][coords.y];
+      if (gridObject !== undefined){
+        const {ship, shipGridIndex} = gridObject;
+        this.goodHits.push(coords);
+        return this.ships[ship].hit(shipGridIndex) ? 'hit' : 'alreadyHit' ;
+      }
+      this.badHits.push(coords);
+      return 'miss';
+      
+    } 
+    return 'alreadyHit';
+    }
+    get allShipsSunk(){
+      return this.ships.every(ship => ship.sunk === true);
+    }
+  static returnCoordsBetweenPoints(coords){
+    const diffX = Math.abs(coords.x1 - coords.x2);
+    const diffY = Math.abs(coords.y1 - coords.y2);
+    let points;
+    if (diffX === 0){
+      const yArray = Gameboard.range(coords.y1, coords.y2);
+      points = yArray.map((yPoint)=> ({"x":coords.x1, "y":yPoint}))
+    } else if (diffY === 0) {
+      const xArray = Gameboard.range(coords.x1, coords.x2);
+      points = xArray.map((xPoint)=> ({"y":coords.y1, "x":xPoint}))
+    }
+    return points;
+  }
+
+  static range(start, end) {
+    const smallest = start > end ? end : start;
+    const largest = start > end ? start : end
+    return Array(largest - smallest + 1).fill().map((_, idx) => smallest + idx)
+  }
+}
+
+export default Gameboard;
---
+++ src/Modules/Templates/Gameboard/Gameboard.test.js
@@ -0,0 +1,81 @@
+import Gameboard from "./Gameboard.js"
+
+let gameboard;
+
+beforeEach(() => {
+  gameboard = new Gameboard()
+});
+
+function placeShip(){
+  let coords = {x1:0, y1:0, x2:0, y2:2}
+  gameboard.placeShip(coords)
+  return gameboard.board
+}
+
+test('should ', () => {
+  expect(placeShip()).toEqual([[{'ship':0, 'shipGridIndex':0},{'ship':0, 'shipGridIndex':1},{'ship':0, 'shipGridIndex':2}],[,, ],[, , ]]);
+});
+
+
+function placeShipAndGoodAttack(){
+  let shipCoords = {'x1':0, 'y1':0, 'x2':0, 'y2':2};
+  gameboard.placeShip(shipCoords);
+  let attackCoords = {'x':0, 'y':0};
+  return gameboard.receiveAttack(attackCoords);
+}
+
+test('Receive good attack', () => {
+  expect(placeShipAndGoodAttack()).toBe('hit');
+});
+
+function placeShipAndMissedAttack(){
+  let shipCoords = {'x1':0, 'y1':0, 'x2':0, 'y2':2};
+  gameboard.placeShip(shipCoords);
+  let attackCoords = {'x':1, 'y':0};
+  return  gameboard.receiveAttack(attackCoords);
+
+}
+
+test('Receive missing attack', () => {
+  expect(placeShipAndMissedAttack()).toBe('miss');
+});
+
+function placeShipAndBadAttack(){
+  let shipCoords = {'x1':0, 'y1':0, 'x2':0, 'y2':2};
+  gameboard.placeShip(shipCoords);
+  let attackCoords = {'x':0, 'y':0};
+  gameboard.receiveAttack(attackCoords);
+  return  gameboard.receiveAttack(attackCoords);
+
+}
+
+test('Receive bad attack', () => {
+  expect(placeShipAndBadAttack()).toBe('alreadyHit');
+});
+
+function hitShipCheckSink(){
+  let shipCoords = {'x1':0, 'y1':0, 'x2':0, 'y2':1};
+  gameboard.placeShip(shipCoords);
+  let attackCoords = {'x':0, 'y':0};
+  gameboard.receiveAttack(attackCoords);
+  return gameboard.allShipsSunk
+
+}
+
+test('hit ship check sink ', () => {
+  expect(hitShipCheckSink()).toBe(false);
+});
+
+function sinkShipCheckSink(){
+  let shipCoords = {'x1':0, 'y1':0, 'x2':0, 'y2':1};
+  gameboard.placeShip(shipCoords);
+  let attackCoords = {'x':0, 'y':0};
+  gameboard.receiveAttack(attackCoords);
+  attackCoords = {'x':0, 'y':1};
+  gameboard.receiveAttack(attackCoords);
+  return gameboard.allShipsSunk
+}
+
+test('sink ship check sink ', () => {
+  expect(sinkShipCheckSink()).toBe(true);
+});
