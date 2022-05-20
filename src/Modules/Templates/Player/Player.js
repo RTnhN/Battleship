c4c00cb93fa7f581fbcb6a2405ec1bd1a4a91757
Zach Strout <zachstrout@gmail.com>
2022-05-20 00:53:04 -0500

    Add Random place ships function

--- src/Modules/Templates/Player/Player.js
+++ src/Modules/Templates/Player/Player.js
@@ -1,33 +1,76 @@
 import Gameboard from "../Gameboard/Gameboard";
 import _ from "lodash";
+import PointsHelper from "../../Util/PointsHelper";
 
 class Player {
-  constructor(type){
+  constructor(type, name) {
     this.gameboard = new Gameboard();
+    this.type = type;
+    this.name = name;
     this.attackHits = [];
     this.attackMisses = [];
-    if (type === "computer"){
+    if (type === "computer") {
       this.attack = this.computerAttack;
     } else if (type === 'human') {
       this.attack = this.humanAttack;
     }
   }
 
-  humanAttack(){
-    return 
+  humanAttack(event) {
+    const point = PointsHelper.DOMStringToObject(event.target.id)
+    return
   }
 
-  computerAttack(){
-    let attack = Player.randomAttackGenerator()  
-    while (_.some(this.attackHits.concat(this.attackMisses), v => _.isEqual(attack, v))){
-      attack = Player.randomAttackGenerator();
+  computerAttack() {
+    let attack = PointsHelper.randomPointGenerator(0, this.gameboard.BOARD_SIZE-1);
+    while (this.attacks.some(v => _.isEqual(attack, v))) {
+      attack = PointsHelper.randomPointGenerator(0, this.gameboard.BOARD_SIZE-1);
     }
     return attack
   }
 
-  static randomAttackGenerator(){
-    return {"x":_.random(1,10,false),"y":_.random(1,10,false)}
+  randomPlaceShips(fleet) {
+    fleet.forEach(this.randomPlaceShip.bind(this))
   }
+
+  randomPlaceShip(ship) {
+    let firstPoint;
+    let secondPoint;
+    let invalid = true;
+    for (let shipInstance = 0; shipInstance < ship.count; shipInstance++) {
+      while (invalid) {
+        firstPoint = PointsHelper.randomPointGenerator(0, this.gameboard.BOARD_SIZE-1);
+        secondPoint = this.generateRandomValidSecondPoint(firstPoint, ship.size)
+        if (!this.newShipAndOldShipsOverlap(firstPoint, secondPoint)) {
+          invalid = false;
+        }
+      }
+      this.gameboard.placeShip(PointsHelper.combineShipPoints(firstPoint, secondPoint));
+      invalid = true;
+    }
+  }
+
+  generateRandomValidSecondPoint(point, size) {
+    const points = [];
+    const boardSize = this.gameboard.BOARD_SIZE;
+    points.push({ 'x': (point.x + size - 1), 'y': point.y })
+    points.push({ 'x': (point.x - size + 1), 'y': point.y })
+    points.push({ 'x': (point.x), 'y': point.y + size - 1 })
+    points.push({ 'x': (point.x), 'y': point.y - size + 1 })
+    const validPoints = points.filter(point => (point.x < boardSize && point.x >= 0 && point.y < boardSize && point.y >= 0))
+    return _.sample(validPoints)
+  }
+
+  newShipAndOldShipsOverlap(firstPoint, secondPoint) {
+    return this.gameboard.shipsCoords.some(shipCoord =>
+      PointsHelper.doLineSegmentsIntersect(firstPoint, secondPoint, ...PointsHelper.breakApartShipPoints(shipCoord))
+    )
+  }
+
+  get attacks(){
+    return this.attackHits.concat(this.attackMisses);
+  }
+
 }
 
 export default Player;
--- src/Modules/Templates/Player/Player.test.js
+++ src/Modules/Templates/Player/Player.test.js
@@ -10,3 +10,35 @@ function createComputerPlayerAndAttack(){
 test('Create computer player and attack', () => {
   expect(createComputerPlayerAndAttack()).toBeDefined();
 });
+
+function randomPlaceShipTest(){
+  player = new Player('computer');
+  const fleetList = [{"size":5,"count":1},{"size":4,"count":2},{"size":3,"count":3}, {"size":2,"count":4}]
+  player.randomPlaceShips(fleetList)
+}
+
+
+function testGenerateRandomValidSecondPoint(){
+  player = new Player("computer");
+  const point = {'x':5, 'y':5};
+  const secondPoint = player.generateRandomValidSecondPoint(point, 3)
+  const diffX = Math.abs(point.x - secondPoint.x);
+  const diffY = Math.abs(point.y - secondPoint.y);
+  return Math.max(diffX, diffY)+1
+}
+
+test('should ', () => {
+  expect(testGenerateRandomValidSecondPoint()).toBe(3);
+});
+
+function computerAttackTwice(){
+  player = new Player("computer");
+  const firstAttack = player.attack()
+  player.attackHits.push(firstAttack);
+  const secondAttack = player.attack()
+  return (firstAttack.x !== secondAttack.x && firstAttack.y !== secondAttack.y)
+}
+
+test('Two attacks not same place ', () => {
+  expect(computerAttackTwice()).toBe(true);
+});
