7b3e56c39323a9382152298803f024ec7dec9b67
Zach Strout <zachstrout@gmail.com>
2022-05-24 23:06:45 -0500

    Incorporate placement of ships
    This is a big, complex change that allows the players to place their
    ships.
    
    This also includes some misc changes like change to the design and
    change to the webpack config file.
    
    Evidentially there needs to be a license for lodash too.

--- src/Modules/DOM/DOM.js
+++ src/Modules/DOM/DOM.js
@@ -28,24 +28,59 @@ class DOM {
 
     DOM.createGrid(this.enemyGridContainer, gridSize);
 
-    this.status = document.createElement('p');
-    this.status.id = 'status';
-    this.status.textContent = 'status';
+    this.placeShipModal = document.createElement('dialog');
+    this.placeShipModal.id = 'placeShipModal';
 
-    this.selfGridContainer = document.createElement('selfGridContainer');
-    this.selfGridContainer.id = 'selfGridContainer';
+    this.placeShipModalContainer = document.createElement('div');
+    this.placeShipModalContainer.id = 'placeShipModalContainer';
 
-    DOM.createGrid(this.selfGridContainer, gridSize);
+    this.placeShipModalGameTitle = document.createElement('h1');
+    this.placeShipModalGameTitle.id = 'placeShipModalGameTitle';
+    this.placeShipModalGameTitle.textContent = 'BATTLESHIP';
+
+    this.placeShipModalTitle = document.createElement('h2');
+    this.placeShipModalTitle.id = 'placeShipModalTitle';
+    this.placeShipModalTitle.textContent = 'Ship Placement';
+    
+    this.placeShipModalSubtitle = document.createElement('p');
+    this.placeShipModalSubtitle.id = 'placeShipModalSubtitle';
+    this.placeShipModalSubtitle.textContent = 'Click two grid squares to place a ship';
+
+    this.makeShipModalShipsContainer();
+    
+    this.placeShipModalGrid = document.createElement('div');
+    this.placeShipModalGrid.id = 'placeShipModalGrid';
+
+    DOM.createGrid(this.placeShipModalGrid, gridSize)
+
+    this.placeShipModalResetButton = document.createElement('button');
+    this.placeShipModalResetButton.id = 'placeShipModalResetButton';
+    this.placeShipModalResetButton.textContent = 'Reset';
+
+    this.placeShipModalCancelButton = document.createElement('button');
+    this.placeShipModalCancelButton.id = 'placeShipModalCancelButton';
+    this.placeShipModalCancelButton.textContent = 'Cancel';
+
+    this.placeShipModalContainer.appendChild(this.placeShipModalGameTitle);
+    this.placeShipModalContainer.appendChild(this.placeShipModalTitle);
+    this.placeShipModalContainer.appendChild(this.placeShipModalSubtitle);
+    this.placeShipModalContainer.appendChild(this.placeShipModalShipsContainer);
+    this.placeShipModalContainer.appendChild(this.placeShipModalGrid);
+    this.placeShipModalContainer.appendChild(this.placeShipModalResetButton);
+    this.placeShipModalContainer.appendChild(this.placeShipModalCancelButton);
+
+    this.placeShipModal.appendChild(this.placeShipModalContainer);
 
     placeholder.appendChild(this.title);
     placeholder.appendChild(this.status);
     placeholder.appendChild(this.selfGridContainer);
     placeholder.appendChild(this.enemyGridContainerTitle);
     placeholder.appendChild(this.enemyGridContainer);
-    placeholder.appendChild(this.status);
-    placeholder.appendChild(this.selfGridContainer);
-
+    placeholder.appendChild(this.placeShipModal);
+  
     contentNode.appendChild(placeholder);
+
+    this.placeShipModalCancelButton.addEventListener('click', () => this.placeShipModal.close())
   }
 
   static createGrid(parentElement, width, height = undefined) {
@@ -110,6 +145,72 @@ class DOM {
         cell.className = '';
         cell.textContent = '';
       });
+  }
+
+  makeShipModalShipsContainer(){
+    this.placeShipModalShipsContainer = document.createElement('div');
+    this.placeShipModalShipsContainer.id = 'placeShipModalShipsContainer';
+
+    this.placeShipModalShipsContainerTitle = document.createElement('h3');
+    this.placeShipModalShipsContainerTitle.id = 'placeShipModalShipsContainerTitle';
+    this.placeShipModalShipsContainerTitle.textContent = 'Ships';
+
+    this.placeShipModalShipsContainerSizeTitle = document.createElement('p');
+    this.placeShipModalShipsContainerSizeTitle.id = 'placeShipModalShipsContainerSizeTitle';
+    this.placeShipModalShipsContainerSizeTitle.textContent = 'Size';
+
+    this.placeShipModalShipsContainerSizeTitle.classList.add('placeShipModal_size');
+
+    this.placeShipModalShipsContainerSizeContainer = document.createElement('div');
+    this.placeShipModalShipsContainerSizeContainer.id = 'placeShipModalShipsContainerSizeContainer';
+
+    const sizeRow = DOM.placeShipModalCreateRow(document.createDocumentFragment(), this.fleetList, 'size');
+
+    this.placeShipModalShipsContainerSizeContainer.appendChild(sizeRow);
+
+    this.placeShipModalShipsContainerCountTitle = document.createElement('p');
+    this.placeShipModalShipsContainerCountTitle.id = 'placeShipModalShipsContainerCountTitle';
+    this.placeShipModalShipsContainerCountTitle.textContent = 'Count';
+    this.placeShipModalShipsContainerCountTitle.classList.add('placeShipModal_count');
+
+    this.placeShipModalShipsContainerCountContainer = document.createElement('div');
+    this.placeShipModalShipsContainerCountContainer.id = 'placeShipModalShipsContainerCountContainer';
+
+    const numberRow = DOM.placeShipModalCreateRow(document.createDocumentFragment(), this.fleetList, 'count');
+
+    this.placeShipModalShipsContainerCountContainer.appendChild(numberRow);
+
+    this.placeShipModalShipsContainer.appendChild(this.placeShipModalShipsContainerTitle);
+    this.placeShipModalShipsContainer.appendChild(this.placeShipModalShipsContainerSizeTitle);
+    this.placeShipModalShipsContainer.appendChild(this.placeShipModalShipsContainerSizeContainer);
+    this.placeShipModalShipsContainer.appendChild(this.placeShipModalShipsContainerCountTitle);
+    this.placeShipModalShipsContainer.appendChild(this.placeShipModalShipsContainerCountContainer);
+  }
+
+  updatePlaceShipModalRows(fleet){
+    DOM.clearPlaceShipModalRow(this.placeShipModalShipsContainerSizeContainer)
+    DOM.placeShipModalCreateRow(this.placeShipModalShipsContainerSizeContainer, fleet, 'size')
+    DOM.clearPlaceShipModalRow(this.placeShipModalShipsContainerCountContainer)
+    DOM.placeShipModalCreateRow(this.placeShipModalShipsContainerCountContainer, fleet, 'count')
+  }
+
+  static placeShipModalCreateRow(node, fleet, type){
+    node = DOM.clearPlaceShipModalRow(node);
+    fleet.forEach((element, index) => {
+      const ship = document.createElement('p');
+      ship.id = `placeShipModal${type}${index}`;
+      ship.classList.add('placeShipModal_'+type);
+      ship.textContent = element[type];
+      node.appendChild(ship);
+    })
+    return node;
+  }
+
+  static clearPlaceShipModalRow(node){
+    while(node.firstChild){
+      node.removeChild(node.firstChild);
+    }
+    return node;
   }
 }
 
--- src/Modules/Game/Game.js
+++ src/Modules/Game/Game.js
@@ -6,8 +6,9 @@ import PointsHelper from "../Util/PointsHelper";
 class Game {
   constructor(contentElement){
     this.fleetList = [{"size":5,"count":1},{"size":4,"count":2},{"size":3,"count":3}, {"size":2,"count":4}]
-    this.startClick;
-    this.endClick;
+    this.startClick = null;
+    this.endClick = null;
+    this.startClickElement;
     this.contentElement = contentElement;
     this.DOM = new DOM(contentElement, this.fleetList);
     this.currentPlayer;
@@ -15,20 +16,21 @@ class Game {
     this.start(); 
   }
   start(){
-    this.currentPlayerSetup = true;
-    this.opposingPlayerSetup = false;
     this.player1 = new Player('human', 'player 1');
     this.player2 = new Player('computer', 'player 2');
     this.currentPlayer = Math.random() > .5 ? this.player1 : this.player2;
     this.opposingPlayer = this.currentPlayer === this.player1 ? this.player2 : this.player1;
-
-    this.player1.randomPlaceShips(this.fleetList);
-    this.player2.randomPlaceShips(this.fleetList);
-    this.DOM.updateDOMFromGameboard(this.player1.gameboard, this.player2.gameboard);
-    this.mainGameLoop();
+    this.reset();
+    this.setup();
   }
+
+  reset(){
+    this.DOM.clearGrids();
+  }
+
   mainGameLoop(){
     let attack;
+    this.DOM.updateDOMFromGameboard(this.player1.gameboard, this.player2.gameboard);
     this.DOM.status.textContent = this.currentPlayer.name;
     if (this.currentPlayer.type === 'human'){
       this.DOM.enemyGridContainer.addEventListener("click", this.executeAttack.bind(this), {once:true});
@@ -44,6 +46,7 @@ class Game {
       if (this.opposingPlayer.gameboard.allShipsSunk){
         alert(this.currentPlayer.name + ' has won')
         this.start();
+        return;
       }
       this.switchPlayers();
       this.mainGameLoop();
@@ -62,6 +65,7 @@ class Game {
       if (this.opposingPlayer.gameboard.allShipsSunk){
         alert(this.currentPlayer.name + ' has won')
         this.start();
+        return;
       }
       this.switchPlayers();
     } else if (this.attackResult === 'miss'){
@@ -73,24 +77,95 @@ class Game {
 
   }
 
-
-
-  // startPlaceShip(event){
-  //   this.startClick = PointsHelper.DOMStringToObject(event.target.id);
-  // }
-
-  // endPlaceShip(){
-
-  // }
-
-  switchPlayers(){
-    this.currentPlayer = this.opposingPlayer
-    this.opposingPlayer = this.currentPlayer === this.player1 ? this.player2 : this.player1
+  setup(){
+    if (this.currentPlayer.setup && this.opposingPlayer.setup){
+      this.mainGameLoop();
+      return;
+    }
+    if (this.currentPlayer.type === 'computer'){
+      this.currentPlayer.randomPlaceShips(this.fleetList);
+      this.currentPlayer.setup = true;
+      this.switchPlayers();
+      this.setup();
+      return;
+    } else {
+      DOM.clearGrid(this.DOM.placeShipModalGrid);
+      this.fleetListClone = structuredClone(this.fleetList);
+      this.DOM.updatePlaceShipModalRows(this.fleetListClone);
+      this.DOM.placeShipModal.showModal();
+      this.DOM.placeShipModalGrid.addEventListener('click', this.placeShipModalGridClicked.bind(this), {once:true});
+    }
   }
 
-  // startGame(){
+  placeShipModalGridClicked(event){
+    if (event.target === event.currentTarget){
+      this.DOM.placeShipModalGrid.addEventListener('click', this.placeShipModalGridClicked.bind(this), {once:true});
+      return;
+    } 
+    if (this.startClick === null){
+      this.startClick = PointsHelper.DOMStringToObject(event.target.id);
+      this.startClickElement = event.target;
+      this.startClickElement.classList.add('coloredShip');
+      this.DOM.placeShipModalGrid.addEventListener('click', this.placeShipModalGridClicked.bind(this), {once:true});
+    } else {
+      this.endClick = PointsHelper.DOMStringToObject(event.target.id);
+      const straightCoords = PointsHelper.makeCoordsStraight(this.startClick, this.endClick)
+      if (straightCoords === null){
+        alert('Hey, why don\'t you stop being a trouble maker. You know exactly what you are doing...');
+        this.startClick = null;
+        this.endClick = null;
+        this.startClickElement.classList.remove('coloredShip');
+        this.DOM.placeShipModalGrid.addEventListener('click', this.placeShipModalGridClicked.bind(this), {once:true});
+        return;
+      }
+      const shipLength = PointsHelper.getShipLen(this.startClick, this.endClick);
+      const removeShipFromFleetStatus = this.removeShipFromFleet(shipLength);
+      if (removeShipFromFleetStatus === 'no ship'){
+        alert('there is not a ship that length, try again');
+        this.startClick = null;
+        this.endClick = null;
+        this.startClickElement.classList.remove('coloredShip');
+        this.DOM.placeShipModalGrid.addEventListener('click', this.placeShipModalGridClicked.bind(this), {once:true});
+        return;
+      } 
+      this.currentPlayer.gameboard.placeShip(straightCoords);
+      DOM.paintShipLocation(this.DOM.placeShipModalGrid, straightCoords);
+      this.DOM.updatePlaceShipModalRows(this.fleetListClone);
+      if (removeShipFromFleetStatus === "all ships placed"){
+        this.currentPlayer.setup = true;
+        this.switchPlayers()
+        this.DOM.placeShipModal.close();
+        this.startClick = null;
+        this.endClick = null;
+        this.setup();
+        return;
+      }
+      this.startClick = null;
+      this.endClick = null;
+      this.DOM.placeShipModalGrid.addEventListener('click', this.placeShipModalGridClicked.bind(this), {once:true});
+    }
+  }
 
-  // }
+  removeShipFromFleet(shipLength){
+    const shipIndex = this.fleetListClone.findIndex(ship => ship.size === shipLength);
+    if (shipIndex === -1){
+      return 'no ship';
+    }
+    if (this.fleetListClone[shipIndex].count === 1){
+      this.fleetListClone.splice(shipIndex, 1);
+    } else {
+      this.fleetListClone[shipIndex].count -= 1;
+    }
+    if (this.fleetListClone.length === 0){
+      return 'all ships placed';
+    }
+    return 'ship placed';
+  }
+
+  switchPlayers(){
+    this.currentPlayer = this.opposingPlayer;
+    this.opposingPlayer = this.currentPlayer === this.player1 ? this.player2 : this.player1;
+  }
 }
 
 export default Game;
